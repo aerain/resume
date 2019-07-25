5c397d347691aad786c63ab980b0e2121d9556b1
aerain <j40f893@gmail.com>
2019-07-25 14:18:58 +0900

    config.js에서 없으면 사라지게

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import '../styles/App.css';
-import { Profile, Career, Skill, Project, Presentation } from '.';
+import { Profile, Career, Skill, Project, Presentation, Education } from '.';
 import config from '../config.js';
 
 function App() {
@@ -9,18 +9,11 @@ function App() {
       <Profile 
         {...config.profile}
       />
-      <Skill 
-        skills={config.skills}
-      />
-      <Career 
-        careers={config.careers}
-      />
-      <Presentation 
-        presentations={config.presentations}
-      />
-      <Project 
-        projects={config.projects}
-      />
+      { config.skills && Array.isArray(config.skills) && <Skill skills={config.skills} />}
+      { config.educations && Array.isArray(config.educations) && <Education educations={config.educations} /> } 
+      { config.careers && Array.isArray(config.careers) && <Career careers={config.careers} /> } 
+      { config.presentations && Array.isArray(config.presentations) && <Presentation presentations={config.presentations} /> }
+      { config.projects && Array.isArray(config.projects) && <Project projects={config.projects} /> }
     </Fragment>
   );
 }
